2480328803abdaec15f713ca3ee30f54ac34b8d7
s3819660 <s3819660@rmit.edu.vn>
2021-11-01 20:51:53 +0700

    + done change dark/light mode, ? currently change theme color

--- app/src/main/java/com/example/rmit_android_assignment1/MenuActivity.java
+++ app/src/main/java/com/example/rmit_android_assignment1/MenuActivity.java
@@ -1,11 +1,19 @@
 package com.example.rmit_android_assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+
+import java.nio.channels.InterruptedByTimeoutException;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -14,11 +22,28 @@ public class MenuActivity extends AppCompatActivity {
 
     String name1 = "";
     String name2 = "";
+    String themeColor = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set theme
+//        Intent intent = getIntent();
+//        themeColor = intent.getStringExtra("theme_color");
+//        try {
+//            if (!themeColor.isEmpty())
+//                setTheme(R.style.AppTheme_blue);
+//        } catch (Exception e) {
+//            System.out.println(e.toString());
+//        }
+
+//        setTheme(R.style.AppTheme_blue);
         setContentView(R.layout.activity_menu);
+
+        // Set status bar color
+//        Window window = getWindow();
+//        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//        window.setStatusBarColor(getResources().getColor(R.color.blue_900));
 
         startBtn = (Button) findViewById(R.id.btn_play);
     }
@@ -27,33 +52,24 @@ public class MenuActivity extends AppCompatActivity {
         Intent intent = new Intent(this, GameActivity.class);
         intent.putExtra("name1", name1);
         intent.putExtra("name2", name2);
-        startActivityForResult(intent, 200); // Go directly to game
+        startActivityForResult(intent, 200); // From menu directly to game
     }
 
     public void goToRegisterActivity(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
-        startActivityForResult(intent, 100); // Go to register
+        startActivityForResult(intent, 100); // From menu to register
     }
 
     public void goToSettingsActivity(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, 300); // From menu to settings
     }
 
     public void goToStart(View view) {
-//        System.out.println("name1=" + name1);
-//        System.out.println("name2=" + name2);
-
         if (name1.isEmpty()) {
             goToRegisterActivity(view);
         } else {
             //TODO: send names to game
-//            Intent intent = getIntent();
-//            name1 = intent.getStringExtra("name1");
-//            name2 = intent.getStringExtra("name2");
-//
-//            System.out.println("name1=" + name1);
-//            System.out.println("name2=" + name2);
             goToGameActivity(view);
         }
     }
@@ -68,12 +84,12 @@ public class MenuActivity extends AppCompatActivity {
                 name1 = intent.getStringExtra("name1");
                 name2 = intent.getStringExtra("name2");
 
-                System.out.println("result OK name1=" + name1);
-                System.out.println("result OK name2=" + name2);
+//                System.out.println("result OK name1=" + name1);
+//                System.out.println("result OK name2=" + name2);
 
             } else {
                 System.out.println(resultCode);
-                System.out.println("result not OK");
+                System.out.println("Result from Game CANCELLED");
             }
         }
 
@@ -82,8 +98,30 @@ public class MenuActivity extends AppCompatActivity {
         if (requestCode == 200) {
 
         }
-    }
 
+        // Menu => Settings => Menu
+        if (requestCode == 300) {
+            if (resultCode == RESULT_OK) {
+                themeColor = intent.getStringExtra("theme_color");
+                System.out.println("Send from settings, themecolor=" + themeColor);
+
+//                setTheme(R.style.AppTheme_blue);
+                // Set status bar color
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(getResources().getColor(R.color.blue_900));
+                setTheme(R.style.AppTheme_blue);
+                Intent recreateIntent = new Intent(this, MenuActivity.class);
+                intent.putExtra("theme_color", themeColor);
+                finish();
+//                startActivity(recreateIntent);
+                startActivity(getIntent());
+            } else {
+                System.out.println(resultCode);
+                System.out.println("Result from settings CANCELLED");
+            }
+        }
+    }
 }
 
     //TODO: make menu animation
--- app/src/main/java/com/example/rmit_android_assignment1/SettingsActivity.java
+++ app/src/main/java/com/example/rmit_android_assignment1/SettingsActivity.java
@@ -1,13 +1,21 @@
 package com.example.rmit_android_assignment1;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
+    String themeColor = "";
+    boolean isNightMode = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,30 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        // Check if default local theme is dark mode
+        isNightMode = false;
+        switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                isNightMode = true;
+//                System.out.println("night mode");
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+                isNightMode = false;
+//                System.out.println("day mode");
+                break;
+        }
+
+        // Load default dark mode
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit().putBoolean("dark_mode", isNightMode).apply();
+        boolean nightModeCheck = sharedPreferences.getBoolean("dark_mode", false);
+//        System.out.println("nighhtmodecheck=" + nightModeCheck);
+
+        //TODO: menu => settings => menu => settings (does not keep the same theme color)
+
+        // Redraw/Update View
+        invalidateOptionsMenu();
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -39,6 +71,29 @@ public class SettingsActivity extends AppCompatActivity {
             super.onBackPressed();
             return true;
         }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSaveBtnClick(View view) {
+        loadSettings();
+        Intent data = new Intent();
+        data.putExtra("theme_color", themeColor);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    private void loadSettings() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        themeColor = sharedPreferences.getString("color_theme", "pink");
+//        System.out.println("colortheme=" + themeColor);
+
+        boolean checkNightMode = sharedPreferences.getBoolean("dark_mode", false);
+        if (checkNightMode != isNightMode) {
+            if (checkNightMode)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
